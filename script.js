7bd69656797ccc31e0f9625d5c0f904c6373eafe
Hamza11067 <hamzakhalid1067@gmail.com>
2024-04-27 21:25:58 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+let tl = gsap.timeline();
+
+tl.from("h1", {
+  y: -30,
+  opacity: 0,
+  duration: 0.5,
+  delay: 0.5,
+});
+
+tl.from("li", {
+  y: -30,
+  opacity: 0,
+  duration: 0.5,
+  stagger: 0.1,
+});
+
+tl.from(".hero", {
+  y: 30,
+  opacity: 0,
+  duration: 0.5,
+  scale: 0.5,
+});
+
+// page 2
+gsap.to(".page2 h1", {
+  transform: "translateX(-73%)",
+  scrollTrigger: {
+    trigger: ".page2",
+    scroller: "body",
+    // markers: true,
+    start: "top 0%",
+    end: "top -150%",
+    scrub: 3,
+    pin: true,
+  },
+});
+
+// page 3
+
+let initialPath = "M 0 100 Q 600 100 1200 100";
+let finalPath = "M 0 100 Q 600 100 1200 100";
+
+let string = document.querySelector(".string")
+
+string.addEventListener("mousemove",function (dets) {
+  finalPath = `M 0 100 Q ${dets.x} ${dets.y} 1200 100`
+
+  gsap.to("svg path", {
+    attr: { d: finalPath},
+    duration: 0.3,
+    ease: "power3.out",
+  })
+})
+
+string.addEventListener("mouseleave", function(){
+  gsap.to("svg path", {
+    attr: { d: initialPath},
+    duration: 1,
+    ease: "elastic.out(1,0.2)",
+  })
+})
